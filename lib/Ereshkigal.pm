6fa19997217ad2adaff2ca4a8c1376b9c42b799b
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-07-22 15:50:19 -0500

    add initial files for Ereshkigal

---
+++ lib/Ereshkigal.pm
@@ -0,0 +1,105 @@
+package Ereshkigal;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Ereshkigal - The great new Ereshkigal!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Ereshkigal;
+
+    my $foo = Ereshkigal->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-ereshkigal at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Ereshkigal>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Ereshkigal
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Ereshkigal>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Ereshkigal>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Ereshkigal>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2023 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of Ereshkigal
